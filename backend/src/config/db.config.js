38a82a0b2adcbec840f78e6f619d12ac3b728712
manoj-ojha04 <manojojha154@gmail.com>
2025-10-16 10:37:45 +0530

    updated-froever

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
